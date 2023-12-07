1cdc2ce7c85a65bcdf4eb24f989fdae4e3567030
Taka <taka@visibleone.com>
2023-12-07 16:43:04 +0630

    foreignObject

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import PDFDownloader from "./components/PDFDownloader";
+import PDFDownloader2 from "./components/PDFDownloader2";
 
 function App() {
   return (
     <>
-      <PDFDownloader />
+      {/* <PDFDownloader /> */}
+      <PDFDownloader2 />
     </>
   );
 }
