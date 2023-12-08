37199b02bb2c32fbe1c7854b5c3fd16bf2bc4ae8
Taka <taka@visibleone.com>
2023-12-08 08:25:29 +0630

    feat: delete non-working code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import PDFDownloader from "./components/PDFDownloader";
-import PDFDownloader2 from "./components/PDFDownloader2";
 
 function App() {
   return (
     <>
       <PDFDownloader />
-      {/* <PDFDownloader2 /> */}
     </>
   );
 }
--- src/components/PDFDownloader.jsx
+++ src/components/PDFDownloader.jsx
@@ -1,23 +1,17 @@
 import { jsPDF } from "jspdf";
-import "svg2pdf.js"; // plugin for jspdf
 import BgImage from "../assets/bg.jpg";
-import QRImage from "../assets/sample.svg";
-import htmlToSvg from "htmlsvg";
 
 const PDFDownloader = () => {
   const download = async () => {
-    // HTML to SVG
     const htmlElem = document.querySelector("#paintArea");
     const width = parseFloat(getComputedStyle(htmlElem).width);
     const height = parseFloat(getComputedStyle(htmlElem).height);
-    console.log(width, height);
 
     const pdfDoc = new jsPDF({
       unit: "px",
       format: [width, height],
     });
 
-    // SVG to PDF
     pdfDoc.html(htmlElem, 0, 0, width, height).then(() => {
       pdfDoc.save("images.pdf");
     });
@@ -28,9 +22,13 @@ const PDFDownloader = () => {
       <div
         style={{ visibility: "hidden", position: "absolute", top: "-9999px", left: "-9999px", pointerEvents: "none" }}
       >
+        {/* template image replica using html */}
         <div id="paintArea" style={{ background: "white", position: "relative" }}>
+          {/* Template Image */}
           <img src={BgImage} style={{ display: "block", minWidth: "100%", minHeight: "100%" }} />
+          {/* QR Code */}
           <img src={BgImage} style={{ width: "100px", position: "absolute", top: "50px", left: "50px" }} />
+          {/* Profile No, */}
           <span
             style={{
               fontSize: "2rem",
